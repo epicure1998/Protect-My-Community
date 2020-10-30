9fff053cfadee54576dd979d211882d4f370e6f7
epicure1998 <qiumk9804@yahoo.com>
2020-10-30 14:15:39 +0800

    Anti-Covid-19 video game for helping people practice hyphenation and study anti-virus acknowledge，抗疫COVID-19英语学习游戏

---
+++ src/main/java/com/qiumengke/protectmycommunity/ProtectmycommunityApplication.java
@@ -0,0 +1,19 @@
+package com.qiumengke.protectmycommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectmycommunityApplication {
+
+    /**
+     * Start the project from here.
+     *
+     * @author Qiu Mengke
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ProtectmycommunityApplication.class, args);
+    }
+
+}
